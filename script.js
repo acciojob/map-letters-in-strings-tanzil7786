1852df8c5e109ff9469ad2eb5343e4aeb3e556f9
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-06-13 20:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function mapLetters(word){
+	const letterMap={};
+	for(let i=0;i<word.length;i++){
+		const letter=word[i];
+		if(!letterMap.hasOwnProperty(letter)){
+			letterMap[letter]=[];
+		}
+		letterMap[letter].push(i);
+	}
+	return letterMap;
+}
